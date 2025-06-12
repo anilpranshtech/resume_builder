86c8151c82a77d215742acbae4030362dc0ef2ed
VedankiPatel <vedanki.patel005@gmail.com>
2025-06-12 14:01:47 +0530

    changes

--- myproject/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Resume, Education, Experience, Skill, Project
-
-admin.site.register(Resume)
-admin.site.register(Education)
-admin.site.register(Experience)
-admin.site.register(Skill)
-admin.site.register(Project)
